2bbd237d67fbe3acc427b86ffc18ccddf23323d0
deveshgupta7 <contact.devesh37@gmail.com>
2024-08-16 03:44:52 +0000

    new changes

--- prod/network/config.tf
+++ prod/network/config.tf
@@ -7,6 +7,6 @@ terraform {
   backend "s3" {
     bucket = "prod-acs"                  // Bucket from where to GET Terraform State
     key    = "network/terraform.tfstate" // Object name in the bucket to GET Terraform State
-    region = "us-east-1"                 // Region where bucket created
+    region = "us-east-1"                 
   }
 }
